6f100adcb02fc6937a2b5a305c056128561512a3
dengzhi01 <dengzhi01@zuoyebang.com>
2022-08-08 18:43:50 +0800

    08.08复习回顾自设计-图的并查集

---
+++ src/main/java/dz/winston/MyUnionFind.java
@@ -0,0 +1,66 @@
+package dz.winston;
+
+import java.util.Arrays;
+
+/**
+ * @Author: EpochDZ
+ * @Date: 18:29 2022/8/8
+ * @Description: 图的 并查集
+ * @Version v1.0
+ */
+public class MyUnionFind {
+    //定义存放顶点信息的数组
+    private int[] root;
+
+    //构造函数
+    public MyUnionFind(int size) {
+        root = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+        }
+    }
+
+    //查找某顶点的根节点
+    public int find(int x) {
+        return root[x];
+    }
+
+    //合并两顶点
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            for (int i = 0; i < root.length; i++) {
+                if (root[i] == rootY) {
+                    root[i] = rootX;
+                }
+            }
+        }
+    }
+
+    // 判断是否连通
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    public static void main(String[] args) {
+        MyUnionFind myUnionFind = new MyUnionFind(10);
+        // 1-2-5-6-7 3-8-9 4
+        myUnionFind.union(1, 2);
+        myUnionFind.union(2, 5);
+        myUnionFind.union(5, 6);
+        myUnionFind.union(6, 7);
+        myUnionFind.union(3, 8);
+        myUnionFind.union(8, 9);
+        System.out.println(myUnionFind.connected(1, 5)); // true
+        System.out.println(myUnionFind.connected(5, 7)); // true
+        System.out.println(myUnionFind.connected(4, 9)); // false
+        // 1-2-5-6-7 3-8-9-4
+        myUnionFind.union(9, 4);
+        System.out.println(myUnionFind.connected(4, 9)); // true
+
+        System.out.println(Arrays.toString(myUnionFind.root));
+    }
+
+
+}
